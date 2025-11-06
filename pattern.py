eea5dcfd0d1466e0e108179d4ff20b01f1b652bd
Marvin <marvinbernard2018@gmail.com>
2025-11-06 10:54:26 +0300

    Patterns using python

---
+++ pattern.py
@@ -0,0 +1,92 @@
+# Solving pattern program in Python
+
+# A square Pattern
+
+n = 5
+print("\nA square Pattern")
+print()
+for i in range(5):
+    for j in range(5):
+        print("*", end=" ")
+    print()
+    
+# Increasing Triangle Pattern
+n = 5
+print("\nIncreasing Triangle Pattern")
+print()
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+
+# Decreasing Triangle Pattern
+n = 5
+print("\nDecreasing Triangle Pattern")
+print()
+for i in range(n):
+    for j in range(i,n):
+        print("*", end=" ") 
+    print()
+    
+# Right sided triangle
+
+n = 5
+print("\nRight Sided Triangle")
+print()
+for i in range(n):
+    
+    for j in range(i,n):
+        print(' ', end=' ')
+        
+    for j in range(i+1):
+        print('*', end=" ")
+        
+    print()
+    
+# Inverted right sided triangle
+
+n = 5
+print('\nInverted right sided triangle')
+print()
+for i in range(n):
+    
+    for j in range(i+1):
+        print(' ', end=' ')
+    
+    for j in range(i,n):
+        print('*', end=' ')
+    
+    print()
+    
+# Hill Pattern
+
+n = 5
+print("\nHill Pattern")
+print()
+for i in range(n):
+    
+    for j in range(i,n):
+        print(' ', end=' ')
+        
+    for j in range(i):
+        print('*', end=' ')
+        
+    for j in range(i+1):
+        print('*', end=' ')
+    
+    print()
+    
+# Reverse Hil Pattern
+n = 5
+print("\nReverse Hil Pattern")
+print()
+for i in range(n):
+    for j in range(i+1):
+        print(' ', end=' ')
+    
+    for j in range(i,n-1):
+        print('*', end=' ')
+        
+    for j in range(i,n):
+        print('*', end=' ')
+    print()
